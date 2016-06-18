79f3488f1d9ab897c68ba9c48f5dcaddd90b09d1
Cristobal Raul Romero Inestroza <crisonic123@hotmail.com>
2016-06-18 17:54:10 -0600

    intento de hilo

--- main.cpp
+++ main.cpp
@@ -6,17 +6,32 @@
 #include "recover.h"
 #include <vector>
 #include <string>
+#include <cstdlib>
+#include <ctime>
+
+#include <thread>
+#include <iostream>
+#include <utility>
+#include <chrono>
+#include <functional>
+#include <atomic>
+using namespace std;
 using std::string;
 using std::vector;
+void hilo();
 void limpiar();
-void charmeleon();
-void frogadier();
+void limpiar_space(int,int,int,int);
+void charmeleon(int);
+void frogadier(int);
 void pokebola();
+void Combate(Pokemon,Pokemon);
 vector<Pokemon*> generar_pokemons();
 vector<Move*> generar_moves(string);
+vector<Move*> oponent_moves(string);
 int main(int argc, char const *argv[])
 {
 	initscr();
+	srand(time(NULL));
 	noecho();
 	start_color();
 	init_pair(1,COLOR_WHITE,COLOR_BLACK);
@@ -96,8 +111,7 @@ int main(int argc, char const *argv[])
 			pokebola();
 			attron(COLOR_PAIR(8));
 			mvprintw(0,(y/2)-20,"(presione cualquier tecla para continuar)");
-			mvprintw(20,(y/2)-20,"ahora se le mostrara la lista de movimientos que podra elegir");
-			mvprintw(21,(y/2)-20,"por defecto su pokemon ya conoce Takle de ataque solo podra conocer 3 mas");
+			mvprintw(20,(y/2)-20,"ahora se le mostrara la lista de movimientos que podra elegir,solo podra elegir 4");
 			avanzar=getch();
 			attroff(COLOR_PAIR(8));	
 			while(revision==0){
@@ -134,7 +148,13 @@ int main(int argc, char const *argv[])
 				attroff(COLOR_PAIR(8));
 			}
 			player.setMoves(playerMoves);
-
+			int oponent=rand()%pokemons.size();
+			Pokemon oponente=Pokemon(pokemons[oponent]);
+			oponente.setMoves(oponent_moves(oponente.getTipo()));
+			thread main(hilo);
+			thread run(Combate);
+			Combate(player,oponente);
+			run.join();
 			refresh();
 		}else if (enter==98)
 		{
@@ -146,15 +166,68 @@ int main(int argc, char const *argv[])
 	endwin();
 	return 0;
 }
+void Combate(Pokemon player,Pokemon cpu){
+
+}
+void hilo(){
+	int x,y,z=2;
+	getmaxyx(stdscr,y,x);
+	while(true){
+		attron(COLOR_PAIR(2));
+		limpiar_space(0,0,23,46);
+		limpiar_space(0,x-46,23,x);
+		attroff(COLOR_PAIR(2));
+		if((z%2)==0){
+			charmeleon(0);
+			frogadier(0);
+		}else{
+			charmeleon(1);
+			frogadier(1);
+		}
+		z++;
+		refresh();
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	}
+}
+void limpiar_space(int y1,int x1,int y2,int x2){
+	for (int i = y1; i <=y2; ++i)
+	{
+		for (int j = x1; j <= x2; ++i)
+		{
+			mvprintw(i,j," ");
+		}
+	}
+}
+vector<Move*> oponent_moves(string tipo){
+	vector<Move*> moves;
+	if(tipo=="Agua"){
+		moves.push_back(new Proteccion("Protec","Normal",50,5,"movimiento que protege de todo ataque al usuario"));
+		moves.push_back(new Recover("Recover","Normal",50,5,"movimientoque recupera vida del usuario(falla si el usuario no tiene dano alguno)"));
+		moves.push_back(new Ataque("Aerial ace","Volador",100,8,"ataque aereo que nunca falla"));
+		moves.push_back(new Ataque("Water pulse","Agua",99,5,"ataque que causa una onda acuatica que golpea al oponente"));
+	}else if(tipo=="Fuego"){
+		moves.push_back(new Proteccion("Protec","Normal",50,5,"movimiento que protege de todo ataque al usuario"));
+		moves.push_back(new Recover("Recover","Normal",50,5,"movimientoque recupera vida del usuario(falla si el usuario no tiene dano alguno)"));
+		moves.push_back(new Ataque("Thunder puch","Electrico",95,8,"ataque que causa una descarga electrica de un golpe"));
+		moves.push_back(new Ataque("Flamethrower","Fuego",99,5,"ataque que lanza fuego al oponente"));
+	}else{
+		moves.push_back(new Proteccion("Protec","Normal",50,5,"movimiento que protege de todo ataque al usuario"));
+		moves.push_back(new Recover("Recover","Normal",50,5,"movimientoque recupera vida del usuario(falla si el usuario no tiene dano alguno)"));
+		moves.push_back(new Ataque("Rock tomb","Roca",90,10,"ataque que tumba rocas sobre el oponente"));
+		moves.push_back(new Ataque("Leaf blade","Hoja",99,5,"ataque lanza hojas afiladas contra el oponente"));
+	}
+}
 vector<Move*> generar_moves(string tipo){
 	vector<Move*> moves;
 	if(tipo=="Agua"){
+		moves.push_back(new Ataque("Takle","Normal",100,15,"ataque simple que baja la vida del oponente con respecto a tu poder de ataque"));	
 		moves.push_back(new Ataque("Rock tomb","Roca",90,10,"ataque que tumba rocas sobre el oponente"));
 		moves.push_back(new Ataque("Aerial ace","Volador",100,8,"ataque aereo que nunca falla"));
 		moves.push_back(new Ataque("Water pulse","Agua",99,5,"ataque que causa una onda acuatica que golpea al oponente"));
 		moves.push_back(new Proteccion("Protec","Normal",50,5,"movimiento que protege de todo ataque al usuario"));
 		moves.push_back(new Recover("Recover","Normal",50,5,"movimientoque recupera vida del usuario(falla si el usuario no tiene dano alguno)"));
 	}else if(tipo=="Fuego"){
+		moves.push_back(new Ataque("Takle","Normal",100,15,"ataque simple que baja la vida del oponente con respecto a tu poder de ataque"));		
 		moves.push_back(new Ataque("Thunder puch","Electrico",95,8,"ataque que causa una descarga electrica de un golpe"));
 		moves.push_back(new Ataque("Rock tomb","Roca",90,10,"ataque que tumba rocas sobre el oponente"));
 		moves.push_back(new Ataque("Aerial ace","Volador",100,8,"ataque aereo que nunca falla"));
@@ -162,6 +235,7 @@ vector<Move*> generar_moves(string tipo){
 		moves.push_back(new Proteccion("Protec","Normal",50,5,"movimiento que protege de todo ataque al usuario"));
 		moves.push_back(new Recover("Recover","Normal",50,5,"movimientoque recupera vida del usuario(falla si el usuario no tiene dano alguno)"));
 	}else{
+		moves.push_back(new Ataque("Takle","Normal",100,15,"ataque simple que baja la vida del oponente con respecto a tu poder de ataque"));	
 		moves.push_back(new Ataque("Thunder puch","Electrico",95,8,"ataque que causa una descarga electrica de un golpe"));
 		moves.push_back(new Ataque("Rock tomb","Roca",90,10,"ataque que tumba rocas sobre el oponente"));
 		moves.push_back(new Ataque("Aerial ace","Volador",100,8,"ataque aereo que nunca falla"));
@@ -220,246 +294,246 @@ void pokebola(){
 	attroff(COLOR_PAIR(5));
 	move(0,0);
 }
-void charmeleon(){
+void charmeleon(int m){
 attron(COLOR_PAIR(4));
-	mvprintw(2,12,"  ");
-	mvprintw(3,12,"    ");
-	mvprintw(4,10,"      ");
-	mvprintw(5,10,"      ");
-	mvprintw(6,14,"  ");
+	mvprintw(2+m,12,"  ");
+	mvprintw(3+m,12,"    ");
+	mvprintw(4+m,10,"      ");
+	mvprintw(5+m,10,"      ");
+	mvprintw(6+m,14,"  ");
 	attroff(COLOR_PAIR(4));
 	attron(COLOR_PAIR(1));
-	mvprintw(1,12,"  ");
-	mvprintw(2,10,"  ");
-	mvprintw(2,14,"  ");
-	mvprintw(3,10,"  ");
-	mvprintw(3,16,"  ");
-	mvprintw(4,16,"  ");
-	mvprintw(5,16,"  ");
-	mvprintw(6,16,"  ");
-	mvprintw(4,8,"  ");
-	mvprintw(5,8,"  ");
-	mvprintw(6,10,"  ");
-	mvprintw(7,6,"    ");
-	mvprintw(8,4,"    ");
-	mvprintw(9,2,"  ");
-	mvprintw(10,0,"  ");
-	mvprintw(11,0,"  ");
-	mvprintw(12,0,"  ");
-	mvprintw(13,0,"  ");
-	mvprintw(7,14,"  ");
-	mvprintw(8,12,"  ");
-	mvprintw(9,10,"  ");
-	mvprintw(10,8,"  ");
-	mvprintw(11,8,"  ");
-	mvprintw(12,10,"                                ");
-	mvprintw(11,14,"              ");
-	mvprintw(11,30,"              ");
-	mvprintw(10,18,"          ");
-	mvprintw(10,30,"              ");
-	mvprintw(9,20,"                      ");
-	mvprintw(8,20,"                    ");
-	mvprintw(7,22,"                  ");
-	mvprintw(6,24,"              ");
-	mvprintw(5,24,"            ");
-	mvprintw(4,22,"          ");
-	mvprintw(3,22,"        ");
-	mvprintw(2,24,"    ");
-	mvprintw(13,10,"                              ");
-	mvprintw(13,2,"  ");
-	mvprintw(14,2,"                                        ");
-	mvprintw(15,4,"              ");
-	mvprintw(15,20,"          ");
-	mvprintw(15,34,"      ");
-	mvprintw(15,42,"  ");
-	mvprintw(16,6,"                      ");
-	mvprintw(16,36,"  ");
-	mvprintw(16,40,"    ");
-	mvprintw(17,8,"            ");
-	mvprintw(17,22,"        ");
-	mvprintw(17,38,"    ");
-	mvprintw(18,8,"                    ");
-	mvprintw(18,30,"  ");
-	mvprintw(19,10,"                    ");
-	mvprintw(20,10,"          ");
-	mvprintw(21,10,"  ");
-	mvprintw(21,18,"  ");
-	mvprintw(22,12,"      ");
+	mvprintw(1+m,12,"  ");
+	mvprintw(2+m,10,"  ");
+	mvprintw(2+m,14,"  ");
+	mvprintw(3+m,10,"  ");
+	mvprintw(3+m,16,"  ");
+	mvprintw(4+m,16,"  ");
+	mvprintw(5+m,16,"  ");
+	mvprintw(6+m,16,"  ");
+	mvprintw(4+m,8,"  ");
+	mvprintw(5+m,8,"  ");
+	mvprintw(6+m,10,"  ");
+	mvprintw(7+m,6,"    ");
+	mvprintw(8+m,4,"    ");
+	mvprintw(9+m,2,"  ");
+	mvprintw(10+m,0,"  ");
+	mvprintw(11+m,0,"  ");
+	mvprintw(12+m,0,"  ");
+	mvprintw(13+m,0,"  ");
+	mvprintw(7+m,14,"  ");
+	mvprintw(8+m,12,"  ");
+	mvprintw(9+m,10,"  ");
+	mvprintw(10+m,8,"  ");
+	mvprintw(11+m,8,"  ");
+	mvprintw(12+m,10,"                                ");
+	mvprintw(11+m,14,"              ");
+	mvprintw(11+m,30,"              ");
+	mvprintw(10+m,18,"          ");
+	mvprintw(10+m,30,"              ");
+	mvprintw(9+m,20,"                      ");
+	mvprintw(8+m,20,"                    ");
+	mvprintw(7+m,22,"                  ");
+	mvprintw(6+m,24,"              ");
+	mvprintw(5+m,24,"            ");
+	mvprintw(4+m,22,"          ");
+	mvprintw(3+m,22,"        ");
+	mvprintw(2+m,24,"    ");
+	mvprintw(13+m,10,"                              ");
+	mvprintw(13+m,2,"  ");
+	mvprintw(14+m,2,"                                        ");
+	mvprintw(15+m,4,"              ");
+	mvprintw(15+m,20,"          ");
+	mvprintw(15+m,34,"      ");
+	mvprintw(15+m,42,"  ");
+	mvprintw(16+m,6,"                      ");
+	mvprintw(16+m,36,"  ");
+	mvprintw(16+m,40,"    ");
+	mvprintw(17+m,8,"            ");
+	mvprintw(17+m,22,"        ");
+	mvprintw(17+m,38,"    ");
+	mvprintw(18+m,8,"                    ");
+	mvprintw(18+m,30,"  ");
+	mvprintw(19+m,10,"                    ");
+	mvprintw(20+m,10,"          ");
+	mvprintw(21+m,10,"  ");
+	mvprintw(21+m,18,"  ");
+	mvprintw(22+m,12,"      ");
 	attroff(COLOR_PAIR(1));
 	attron(COLOR_PAIR(3));
-	mvprintw(6,12,"  ");
-	mvprintw(7,12,"  ");
-	mvprintw(14,24,"    ");
-	mvprintw(15,22,"      ");
-	mvprintw(16,24,"  ");
-	mvprintw(17,24,"  ");
-	mvprintw(18,22,"  ");
+	mvprintw(6+m,12,"  ");
+	mvprintw(7+m,12,"  ");
+	mvprintw(14+m,24,"    ");
+	mvprintw(15+m,22,"      ");
+	mvprintw(16+m,24,"  ");
+	mvprintw(17+m,24,"  ");
+	mvprintw(18+m,22,"  ");
 	attroff(COLOR_PAIR(3));
 	attron(COLOR_PAIR(5));
-	mvprintw(7,10,"  ");
-	mvprintw(8,8,"    ");
-	mvprintw(9,4,"      ");
-	mvprintw(10,2,"      ");
-	mvprintw(11,2,"      ");
-	mvprintw(12,2,"        ");
-	mvprintw(12,14,"            ");
-	mvprintw(12,28,"            ");
-	mvprintw(13,4,"      ");
-	mvprintw(13,12,"      ");
-	mvprintw(13,20,"        ");
-	mvprintw(13,32,"    ");
-	mvprintw(14,4,"              ");
-	mvprintw(14,20,"    ");
-	mvprintw(14,36,"    ");
-	mvprintw(15,6,"  ");
-	mvprintw(15,10,"        ");
-	mvprintw(15,36,"    ");
-	mvprintw(16,10,"            ");
-	mvprintw(16,40,"  ");
-	mvprintw(17,10,"          ");
-	mvprintw(18,12,"      ");
-	mvprintw(18,26,"  ");
-	mvprintw(19,12,"        ");
-	mvprintw(20,14,"  ");
-	mvprintw(21,14,"  ");
-	mvprintw(11,18,"    ");
-	mvprintw(11,24,"  ");
-	mvprintw(11,34,"        ");
-	mvprintw(10,22,"      ");
-	mvprintw(10,32,"          ");
-	mvprintw(9,22,"      ");
-	mvprintw(9,30,"          ");
-	mvprintw(8,22,"                ");
-	mvprintw(7,24,"              ");
-	mvprintw(6,28,"    ");
-	mvprintw(6,34,"  ");
-	mvprintw(5,26,"    ");
-	mvprintw(4,24,"      ");
-	mvprintw(3,24,"    ");
+	mvprintw(7+m,10,"  ");
+	mvprintw(8+m,8,"    ");
+	mvprintw(9+m,4,"      ");
+	mvprintw(10+m,2,"      ");
+	mvprintw(11+m,2,"      ");
+	mvprintw(12+m,2,"        ");
+	mvprintw(12+m,14,"            ");
+	mvprintw(12+m,28,"            ");
+	mvprintw(13+m,4,"      ");
+	mvprintw(13+m,12,"      ");
+	mvprintw(13+m,20,"        ");
+	mvprintw(13+m,32,"    ");
+	mvprintw(14+m,4,"              ");
+	mvprintw(14+m,20,"    ");
+	mvprintw(14+m,36,"    ");
+	mvprintw(15+m,6,"  ");
+	mvprintw(15+m,10,"        ");
+	mvprintw(15+m,36,"    ");
+	mvprintw(16+m,10,"            ");
+	mvprintw(16+m,40,"  ");
+	mvprintw(17+m,10,"          ");
+	mvprintw(18+m,12,"      ");
+	mvprintw(18+m,26,"  ");
+	mvprintw(19+m,12,"        ");
+	mvprintw(20+m,14,"  ");
+	mvprintw(21+m,14,"  ");
+	mvprintw(11+m,18,"    ");
+	mvprintw(11+m,24,"  ");
+	mvprintw(11+m,34,"        ");
+	mvprintw(10+m,22,"      ");
+	mvprintw(10+m,32,"          ");
+	mvprintw(9+m,22,"      ");
+	mvprintw(9+m,30,"          ");
+	mvprintw(8+m,22,"                ");
+	mvprintw(7+m,24,"              ");
+	mvprintw(6+m,28,"    ");
+	mvprintw(6+m,34,"  ");
+	mvprintw(5+m,26,"    ");
+	mvprintw(4+m,24,"      ");
+	mvprintw(3+m,24,"    ");
 	attroff(COLOR_PAIR(5));
 	move(0,0);
 }
-void frogadier(){
+void frogadier(int m){
 	int x,y;
 	getmaxyx(stdscr,x,y);
 	attron(COLOR_PAIR(1));
-	mvprintw(5,y-2,"  ");
-	mvprintw(6,y-2,"  ");
-	mvprintw(4,y-6,"    ");
-	mvprintw(5,y-8,"  ");
-	mvprintw(6,y-10,"    ");
-	mvprintw(7,y-14,"    ");
-	mvprintw(6,y-18,"    ");
-	mvprintw(7,y-6,"    ");
-	mvprintw(8,y-6,"  ");
-	mvprintw(9,y-8,"    ");
-	mvprintw(10,y-10,"    ");
-	mvprintw(10,y-16,"  ");
-	mvprintw(11,y-16,"      ");
-	mvprintw(1,y-34,"  ");
-	mvprintw(2,y-36,"      ");
-	mvprintw(2,y-24,"      ");
-	mvprintw(3,y-38,"        ");
-	mvprintw(3,y-28,"          ");
-	mvprintw(4,y-38,"                    ");
-	mvprintw(5,y-40,"                    ");
-	mvprintw(6,y-40,"                    ");
-	mvprintw(7,y-42,"                        ");
-	mvprintw(8,y-44,"  ");
-	mvprintw(8,y-40,"                      ");
-	mvprintw(9,y-44,"      ");
-	mvprintw(9,y-36,"                  ");
-	mvprintw(10,y-42,"                      ");
-	mvprintw(11,y-40,"                  ");
-	mvprintw(12,y-36,"          ");
-	mvprintw(13,y-36,"  ");
-	mvprintw(13,y-22,"  ");
-	mvprintw(13,y-18,"  ");
-	mvprintw(12,y-18,"  ");
-	mvprintw(14,y-38,"        ");
-	mvprintw(14,y-24,"          ");
-	mvprintw(15,y-38,"                          ");
-	mvprintw(16,y-40,"                            ");
-	mvprintw(17,y-44,"          ");
-	mvprintw(17,y-32,"                  ");
-	mvprintw(18,y-46,"  ");
-	mvprintw(18,y-42,"      ");
-	mvprintw(18,y-30,"                  ");
-	mvprintw(19,y-46,"  ");
-	mvprintw(19,y-40,"  ");
-	mvprintw(19,y-36,"  ");
-	mvprintw(19,y-28,"                  ");
-	mvprintw(20,y-44,"    ");
-	mvprintw(20,y-38,"    ");
-	mvprintw(20,y-30,"  ");
-	mvprintw(20,y-26,"        ");
-	mvprintw(20,y-16,"    ");
-	mvprintw(21,y-30,"  ");
-	mvprintw(21,y-24,"  ");
-	mvprintw(21,y-20,"  ");
-	mvprintw(22,y-28,"    ");
-	mvprintw(22,y-22,"  ");
+	mvprintw(5+m,y-2,"  ");
+	mvprintw(6+m,y-2,"  ");
+	mvprintw(4+m,y-6,"    ");
+	mvprintw(5+m,y-8,"  ");
+	mvprintw(6+m,y-10,"    ");
+	mvprintw(7+m,y-14,"    ");
+	mvprintw(6+m,y-18,"    ");
+	mvprintw(7+m,y-6,"    ");
+	mvprintw(8+m,y-6,"  ");
+	mvprintw(9+m,y-8,"    ");
+	mvprintw(10+m,y-10,"    ");
+	mvprintw(10+m,y-16,"  ");
+	mvprintw(11+m,y-16,"      ");
+	mvprintw(1+m,y-34,"  ");
+	mvprintw(2+m,y-36,"      ");
+	mvprintw(2+m,y-24,"      ");
+	mvprintw(3+m,y-38,"        ");
+	mvprintw(3+m,y-28,"          ");
+	mvprintw(4+m,y-38,"                    ");
+	mvprintw(5+m,y-40,"                    ");
+	mvprintw(6+m,y-40,"                    ");
+	mvprintw(7+m,y-42,"                        ");
+	mvprintw(8+m,y-44,"  ");
+	mvprintw(8+m,y-40,"                      ");
+	mvprintw(9+m,y-44,"      ");
+	mvprintw(9+m,y-36,"                  ");
+	mvprintw(10+m,y-42,"                      ");
+	mvprintw(11+m,y-40,"                  ");
+	mvprintw(12+m,y-36,"          ");
+	mvprintw(13+m,y-36,"  ");
+	mvprintw(13+m,y-22,"  ");
+	mvprintw(13+m,y-18,"  ");
+	mvprintw(12+m,y-18,"  ");
+	mvprintw(14+m,y-38,"        ");
+	mvprintw(14+m,y-24,"          ");
+	mvprintw(15+m,y-38,"                          ");
+	mvprintw(16+m,y-40,"                            ");
+	mvprintw(17+m,y-44,"          ");
+	mvprintw(17+m,y-32,"                  ");
+	mvprintw(18+m,y-46,"  ");
+	mvprintw(18+m,y-42,"      ");
+	mvprintw(18+m,y-30,"                  ");
+	mvprintw(19+m,y-46,"  ");
+	mvprintw(19+m,y-40,"  ");
+	mvprintw(19+m,y-36,"  ");
+	mvprintw(19+m,y-28,"                  ");
+	mvprintw(20+m,y-44,"    ");
+	mvprintw(20+m,y-38,"    ");
+	mvprintw(20+m,y-30,"  ");
+	mvprintw(20+m,y-26,"        ");
+	mvprintw(20+m,y-16,"    ");
+	mvprintw(21+m,y-30,"  ");
+	mvprintw(21+m,y-24,"  ");
+	mvprintw(21+m,y-20,"  ");
+	mvprintw(22+m,y-28,"    ");
+	mvprintw(22+m,y-22,"  ");
 	attroff(COLOR_PAIR(1));
 	attron(COLOR_PAIR(6));
-	mvprintw(5,y-28,"  ");
-	mvprintw(6,y-30,"      ");
-	mvprintw(7,y-30,"      ");
-	mvprintw(8,y-30,"  ");
-	mvprintw(9,y-42,"    ");
-	mvprintw(10,y-40,"    ");
-	mvprintw(11,y-36,"          ");
-	mvprintw(10,y-24,"  ");
-	mvprintw(14,y-36,"  ");
-	mvprintw(15,y-36,"  ");
-	mvprintw(16,y-38,"  ");
-	mvprintw(17,y-38,"  ");
-	mvprintw(15,y-32,"  ");
-	mvprintw(16,y-32,"  ");
-	mvprintw(17,y-30,"  ");
-	mvprintw(16,y-28,"    ");
-	mvprintw(14,y-22,"  ");
-	mvprintw(15,y-22,"  ");
-	mvprintw(16,y-22,"  ");
-	mvprintw(17,y-24,"    ");
-	mvprintw(18,y-24,"  ");
-	mvprintw(15,y-18,"    ");
-	mvprintw(16,y-18,"    ");
-	mvprintw(17,y-18,"  ");
-	mvprintw(18,y-20,"    ");
-	mvprintw(19,y-16,"  ");
+	mvprintw(5+m,y-28,"  ");
+	mvprintw(6+m,y-30,"      ");
+	mvprintw(7+m,y-30,"      ");
+	mvprintw(8+m,y-30,"  ");
+	mvprintw(9+m,y-42,"    ");
+	mvprintw(10+m,y-40,"    ");
+	mvprintw(11+m,y-36,"          ");
+	mvprintw(10+m,y-24,"  ");
+	mvprintw(14+m,y-36,"  ");
+	mvprintw(15+m,y-36,"  ");
+	mvprintw(16+m,y-38,"  ");
+	mvprintw(17+m,y-38,"  ");
+	mvprintw(15+m,y-32,"  ");
+	mvprintw(16+m,y-32,"  ");
+	mvprintw(17+m,y-30,"  ");
+	mvprintw(16+m,y-28,"    ");
+	mvprintw(14+m,y-22,"  ");
+	mvprintw(15+m,y-22,"  ");
+	mvprintw(16+m,y-22,"  ");
+	mvprintw(17+m,y-24,"    ");
+	mvprintw(18+m,y-24,"  ");
+	mvprintw(15+m,y-18,"    ");
+	mvprintw(16+m,y-18,"    ");
+	mvprintw(17+m,y-18,"  ");
+	mvprintw(18+m,y-20,"    ");
+	mvprintw(19+m,y-16,"  ");
 	attroff(COLOR_PAIR(6));
 	attron(COLOR_PAIR(3));
-	mvprintw(8,y-26,"  ");
-	mvprintw(9,y-30,"  ");
-	mvprintw(9,y-26,"  ");
-	mvprintw(10,y-28,"  ");
+	mvprintw(8+m,y-26,"  ");
+	mvprintw(9+m,y-30,"  ");
+	mvprintw(9+m,y-26,"  ");
+	mvprintw(10+m,y-28,"  ");
 	attroff(COLOR_PAIR(3));
 	attron(COLOR_PAIR(7));
-	mvprintw(17,y-40,"  ");
-	mvprintw(18,y-42,"    ");
-	mvprintw(19,y-24,"  ");
-	mvprintw(20,y-26,"    ");
-	mvprintw(19,y-20,"  ");
-	mvprintw(19,y-14,"  ");
-	mvprintw(2,y-34,"  ");
-	mvprintw(3,y-36,"    ");
-	mvprintw(4,y-36,"      ");
-	mvprintw(5,y-38,"        ");
-	mvprintw(6,y-38,"      ");
-	mvprintw(7,y-38,"  ");
-	mvprintw(7,y-34,"  ");
-	mvprintw(8,y-40,"  ");
-	mvprintw(8,y-36,"    ");
-	mvprintw(9,y-36,"      ");
-	mvprintw(10,y-36,"        ");
-	mvprintw(10,y-26,"  ");
-	mvprintw(3,y-24,"    ");
-	mvprintw(4,y-28,"        ");
-	mvprintw(5,y-26,"    ");
-	mvprintw(6,y-24,"  ");
-	mvprintw(7,y-24,"    ");
-	mvprintw(8,y-24,"    ");
-	mvprintw(9,y-24,"    ");
+	mvprintw(17+m,y-40,"  ");
+	mvprintw(18+m,y-42,"    ");
+	mvprintw(19+m,y-24,"  ");
+	mvprintw(20+m,y-26,"    ");
+	mvprintw(19+m,y-20,"  ");
+	mvprintw(19+m,y-14,"  ");
+	mvprintw(2+m,y-34,"  ");
+	mvprintw(3+m,y-36,"    ");
+	mvprintw(4+m,y-36,"      ");
+	mvprintw(5+m,y-38,"        ");
+	mvprintw(6+m,y-38,"      ");
+	mvprintw(7+m,y-38,"  ");
+	mvprintw(7+m,y-34,"  ");
+	mvprintw(8+m,y-40,"  ");
+	mvprintw(8+m,y-36,"    ");
+	mvprintw(9+m,y-36,"      ");
+	mvprintw(10+m,y-36,"        ");
+	mvprintw(10+m,y-26,"  ");
+	mvprintw(3+m,y-24,"    ");
+	mvprintw(4+m,y-28,"        ");
+	mvprintw(5+m,y-26,"    ");
+	mvprintw(6+m,y-24,"  ");
+	mvprintw(7+m,y-24,"    ");
+	mvprintw(8+m,y-24,"    ");
+	mvprintw(9+m,y-24,"    ");
 	attroff(COLOR_PAIR(7));
 	move(0,0);
 }
